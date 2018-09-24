3ca00ae3f858eacd3f03d6341bad7fdbb652c3ea
David Purcell <dpurcell@lakana.com>
2018-09-24 09:42:09 -0500

    basics of backend app working

---
+++ src/main/java/org/supurcell/todoapp/repositories/TodoRepository.java
@@ -0,0 +1,9 @@
+package org.supurcell.todoapp.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.supurcell.todoapp.models.Todo;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+}
